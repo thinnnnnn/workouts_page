d166d6a316ab9c2fcf9fbfc6653d51939b02daa3
thinnnnnn <118615035+thinnnnnn@users.noreply.github.com>
2024-11-14 11:43:48 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,15 +11,15 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteTitle: 'Li Yang's Workouts Map',
+  siteUrl: 'https://go.eimoo.com',
+  logo: 'https://eimoo.com/service/avatar/0ab4d0eec14aa1dc634d6d75dffe80d9',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'https://eimoo.com',
     },
     {
       name: 'About',
